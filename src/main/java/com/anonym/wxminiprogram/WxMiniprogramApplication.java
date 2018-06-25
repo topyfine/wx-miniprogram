159e993c71262784be043af13a5a35e5b63bf2ee
杨帆 <yangfan@shuangke.net.cn>
2018-06-25 11:37:24 +0800

    Initial commit

---
+++ src/main/java/com/anonym/wxminiprogram/WxMiniprogramApplication.java
@@ -0,0 +1,12 @@
+package com.anonym.wxminiprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxMiniprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxMiniprogramApplication.class, args);
+	}
+}
